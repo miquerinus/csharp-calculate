77b2a84021b3d9f7c1246fd5267a6ffd0d5aef29
miquerinus <miquerinus.br@gmail.com>
2024-03-10 07:00:06 -0300

    upload project calculadora simples csharp 01/01

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+class Program {
+
+    static void Main(String[] args) {
+
+         Console.WriteLine("*********************************************************");
+         Console.WriteLine("*               Calculadora Simples                     *");
+         Console.WriteLine("*********************************************************");
+
+         menu();
+
+         int @operator = int.Parse(Console.ReadLine());
+
+         Console.WriteLine("Informe o 1ª numero: ");
+         int number1 = int.Parse(Console.ReadLine());
+
+         Console.WriteLine();                 
+         
+         Console.WriteLine("Informe o 2ª numero: ");
+         int number2 = int.Parse(Console.ReadLine());
+
+         int result = 0;
+
+         switch(@operator) {
+
+            case 1: result = add(number1, number2);
+            break;
+
+            case 2: result = sub(number1, number2);
+            break;
+
+            case 3: result = mult(number1, number2);
+            break;
+
+            case 4: result = div(number1, number2);
+            break; 
+
+            default: 
+            Console.WriteLine("Error: Opção não valida");
+            break; 
+         
+         }
+
+         string msn = $"O resultado entre {number1} e {number2} é: {result}"; 
+         Console.WriteLine(msn); 
+    }
+
+    public static void menu() {
+         Console.WriteLine("Informe abaixo qual operação matematica desejada: ");
+         Console.WriteLine("1 - Adição: ");
+         Console.WriteLine("2 - Subtração: ");
+         Console.WriteLine("3 - Multiplicação: ");
+         Console.WriteLine("4 - Divisão: ");
+    }
+    
+    public static int add(int x, int y) {
+        return x + y;
+    }
+
+    public static int sub(int x, int y) {
+        return x - y;
+    }
+
+    public static int mult(int x, int y) {
+        return x * y;
+    }
+
+    public static int div(int x, int y) {
+        return x / y;
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+<h2>C-Sharp calculadora</h2>
+
+<P> Objetivo do programa foi desenvolver uma calculadora simples com a linguagem C Sharp rodando dentro de um ecossistema linux, cabe destacar também que, objetivo foi exercitar a pratica da logica de programação.
+Fica claro que a intenção de modularizar o codigo, ja que ele esta dividido em chamadas de funções. Por hora os testes usando a linguagem estão satisfatorios. Pretendo com o tempo criar uma interface mais atraente para essa calculadorai tal como incluir ainda mais funções para que ela seja ainda mais completa. 
+</P>
+
+<h2>Conclusão: </h2>
+
+<p>Como projeto basico consegui cumprir satisfatoriamente o objetivo, e com o conhecimento adiquirido irei desenvolver mais outros algoritmos que atendam necessidades distintas.</p>
